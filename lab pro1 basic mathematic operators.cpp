3bbcb8b280fa66db3d8bf303e33307cd35fd0fe3
Aditya Manohar Mhaismale <aditya.mhaismale24@sanjivani.edu.in>
2024-08-25 22:52:16 +0530

    Add files via upload

---
+++ lab pro1 basic mathematic operators.cpp
@@ -0,0 +1,29 @@
+//this program performs basic mathematic operations
+#include<iostream>
+using namespace std;
+
+int main(){
+  //assigning numbers and operations
+  int first , second , add , subtract , multiply;
+  float divide;
+  
+  //taking inputs
+  cout<<"Please enter two digits :"
+  cin>>first;
+  cin>>second;
+  
+  //assigning operation functions
+  add=first+second;
+  subtract=fist-second;
+  multiply=first*second;
+  divide=first/(float)second;
+  
+  //for printing output 
+  cout<<endl<<"Sum : "<<add;
+  cout<<endl<<"Difference : "<<subtract;
+  cout<<endl<<"Multiplicaton : "<<multiply;
+  cout<<endl<<"Division : "<<divide
+  
+  return 0;
+  
+}
