021e66a2ce61cae2fda91b7e8a1b0e05a7611645
Aditya Manohar Mhaismale <aditya.mhaismale24@sanjivani.edu.in>
2024-09-21 22:39:25 +0530

    Add files via upload

---
+++ lab pro11 bitwise operator.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+  
+  int a=10 , b=4;
+  
+  //Binary AND operator 
+  cout<<"a | b is : "<<(a&b)<<endl;
+  
+  //Binary OR operator
+  cout<<"a | b is :"<<(a|b)<<endl;
+  
+  //Binary XOR operator
+  cout<<"a^b is :"<<(a^b)<<endl;
+  
+  //Left shift operator 
+  cout<<"a<<1 is :"<<(a<<1)<<endl;
+  
+  //Right shift operator
+  cout<<"a>>1 is :"<<(a>>1)<<endl;
+  
+  //Complement operator
+  cout<<"~(a) is "<<~(a)<<endl;
+  
+  return 0;
+  
+}
